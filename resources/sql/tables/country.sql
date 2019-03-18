4fa1a2a194b0595377092b3adffaba29dc71a65c
alexandre <alexandre.cesari@telecomnancy.eu>
2019-03-18 11:33:30 +0100

    mr review

---
+++ resources/sql/tables/country.sql
@@ -0,0 +1,15 @@
+CREATE TABLE COUNTRY 
+(
+	id		NUMBER,
+	id_dst		NUMBER,
+	name		VARCHAR(50),
+	inhabitants	NUMBER,
+	superficy	BINARY_FLOAT,
+
+	CONSTRAINT PK_COUNTRY PRIMARY KEY (id)
+	
+  	CONSTRAINT FK_COUNTRY_DST
+		FOREIGN KEY (id_dst)
+		REFERENCES DST (id),
+    
+)
