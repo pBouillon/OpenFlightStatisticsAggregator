2b6282e4074e6423f0db09a1aab5395c96d42ca2
CESARI Alexandre <alexandre.cesari@telecomnancy.eu>
2019-03-23 12:20:34 +0100

    fix alphabetical order airport.sql

--- resources/sql/tables/airport.sql
+++ resources/sql/tables/airport.sql
@@ -3,11 +3,11 @@ CREATE TABLE AIRPORT
     id          NUMBER          NOT NULL,
     id_city     NUMBER          NOT NULL,
     altitude    BINARY_FLOAT    NOT NULL,
+    id_iata     VARCHAR(3),
+    id_icao     VARCHAR(4),
     latitude    BINARY_FLOAT    NOT NULL,
     longitude   BINARY_FLOAT    NOT NULL,
     name        VARCHAR(150)    NOT NULL,
-    id_iata     VARCHAR(3),
-    id_icao     VARCHAR(4),
 
     CONSTRAINT PK_AIRPORT PRIMARY KEY (id),
 
