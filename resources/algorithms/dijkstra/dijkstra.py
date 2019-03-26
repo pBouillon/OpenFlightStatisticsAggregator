b5b00f9250ba03548211edb612d72b0edc1a6083
BOUILLON Pierre <pierre.bouillon@telecomnancy.eu>
2019-03-26 13:26:06 +0100

    add documentation

--- resources/algorithms/dijkstra/dijkstra.py
+++ resources/algorithms/dijkstra/dijkstra.py
@@ -13,32 +13,44 @@ from typing import Dict, List
 
 
 class Graph:
+    """References Graph
+
+    Minimal implementation of a graph
+    """
+
+    DISPLAY_INDENT = 2
+
     def __init__(self):
         self._graph = dict()
 
     def __str__(self):
-        return json.dumps(self._graph, indent=4)
+        return json.dumps(self._graph, indent=self.DISPLAY_INDENT)
 
     def get_distance(self, source: str, target: str) -> int:
-        """
+        """Get the distance from `source` to `target`
 
-        :param source:
-        :param target:
-        :return:
+        :raise KeyError: on a non-existing edge
+
+        :param source: source edge
+        :param target: target edge
+        :return: the distance between them as an int
         """
         return self._graph[source][target]
 
     def set_distance(self, edge: str, distances_to: Dict[str, int]):
-        """
+        """Add a distance from `edge` to all edges as keys in `distance_to`
 
-        :param edge:
-        :param distances_to:
-        :return:
+        :param edge: source
+        :param distances_to: dict as {target: distance_to_target]
         """
         self._graph[edge] = distances_to
 
     @property
     def edges(self) -> List[str]:
+        """Get all edges
+
+        :return: list of edges
+        """
         return list(self._graph.keys())
 
 
@@ -66,7 +78,12 @@ def get_default_graph() -> Graph:
 
 
 def main():
-    pass
+    graph = get_default_graph()
+    print(graph)
+    print(
+        f'distance from `s` to `A`: '
+        f'{graph.get_distance(source="s", target="A")}'
+    )
 
 
 if __name__ == '__main__':
