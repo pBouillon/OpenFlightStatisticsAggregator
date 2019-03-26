1cae2a8994cd80b8f08c09b6a1db636ff1bb47e6
BOUILLON Pierre <pierre.bouillon@telecomnancy.eu>
2019-03-26 13:16:50 +0100

    add file for minimal dijkstra implementation

---
+++ resources/algorithms/dijkstra/dijkstra.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+    dijkstra
+    --------
+
+    Dijkstra shortest path implementation.
+
+    :authors: Bouillon Pierre, Cesari Alexandre.
+    :licence: MIT, see LICENSE for more details.
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
