9cd8e0cc8fd2c5fb254b1e10738781d01eefbb61
CESARI Alexandre <alexandre.cesari@telecomnancy.eu>
2019-04-01 10:38:39 +0200

    Update plane.sql

--- resources/database/scripts/tables/plane.sql
+++ resources/database/scripts/tables/plane.sql
@@ -1,13 +1,18 @@
-CREATE TABLE PLANE_TYPE
+CREATE TABLE PLANE
 (
-    id          NUMBER  	NOT NULL,
-    type        VARCHAR(50)	NOT NULL,
-    consumption NUMBER,
-    freight     NUMBER,
-    id_iata     VARCHAR(3),
-    id_icao     VARCHAR(4),
-    passengers  NUMBER,
-    speed       NUMBER,
+    id          	NUMBER  	NOT NULL,
+    id_plane_type   NUMBER,
+    name        	VARCHAR(50)	NOT NULL,
+    consumption 	NUMBER,
+    freight     	NUMBER,
+    id_iata     	VARCHAR(3),
+    id_icao     	VARCHAR(4)	NOT NULL,
+    passengers  	NUMBER,
+    speed       	NUMBER,
 
-    CONSTRAINT PK_PLANE_TYPE PRIMARY KEY (id),
+    CONSTRAINT PK_PLANE PRIMARY KEY (id),
+
+    CONSTRAINT FK_PLANE_TYPE
+        FOREIGN KEY (id_plane_type)
+        REFERENCES  PLANE_TYPE(id)
 )
