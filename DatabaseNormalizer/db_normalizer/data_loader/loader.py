9000a1f31035d522afb4f89d919bb602a60b220f
BOUILLON Pierre <pierre.bouillon@telecomnancy.eu>
2019-04-02 21:24:49 +0200

    loader object base structure for load basic tables

---
+++ DatabaseNormalizer/db_normalizer/data_loader/loader.py
@@ -0,0 +1,40 @@
+class Loader:
+
+    def __init__(self):
+        self._tables = {
+            'airline': [],
+            'timezone': [],
+            'plane': [],
+            'plane_type': [],
+            'airway': [],
+            'city': [],
+            'dst': [],
+            'county': [],
+            'use': [],
+            'fly_on': [],
+            'Step_in': [],
+        }
+
+    def load_airport(self):
+        pass
+
+    def load_all(self):
+        pass
+
+    def load_dst(self):
+        pass
+
+    def load_city(self):
+        pass
+
+    def load_country(self):
+        pass
+
+    def load_plane(self):
+        pass
+
+    def load_plane_type(self):
+        pass
+
+    def load_timezone(self):
+        pass
