36135c5e82575ac247dbeb203ff461c72a62297f
CESARI Alexandre <alexandre.cesari@telecomnancy.eu>
2019-04-04 18:38:31 +0200

    Update plane_type.sql

--- resources/database/scripts/tables/plane_type.sql
+++ resources/database/scripts/tables/plane_type.sql
@@ -1,6 +1,6 @@
 CREATE TABLE PLANE_TYPE
 (
-    id          NUMBER  		NOT NULL,
+    id          NUMBER          NOT NULL,
     id_iata     VARCHAR2(3)		NOT NULL,
     type        VARCHAR2(30)	NOT NULL,
 
