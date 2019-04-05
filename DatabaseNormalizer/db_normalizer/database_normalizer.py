58ef6b3909ecf911395e3686da198e38302b9d23
BOUILLON Pierre <pierre.bouillon@telecomnancy.eu>
2019-04-05 09:38:47 +0200

    update version name and improve display

--- DatabaseNormalizer/db_normalizer/database_normalizer.py
+++ DatabaseNormalizer/db_normalizer/database_normalizer.py
@@ -14,7 +14,7 @@ from db_normalizer.csv_handler.normalizer import Normalizer
 from db_normalizer.csv_handler.utils import Dat
 from db_normalizer.data_loader.loader import Loader
 
-__version__ = '1.4.2'
+__version__ = '1.6.2'
 
 
 def show_header() -> None:
@@ -71,6 +71,8 @@ if __name__ == '__main__':
     print('Recorded:')
     print(f'\t{len(loader.airline_records):6}{"":4}Airlines')
     print(f'\t{len(loader.airway_records):6}{"":4}Airways')
+    print(f'\t{len(loader.city_records):6}{"":4}Cities')
+    print(f'\t{len(loader.country_records):6}{"":4}Countries')
     print(f'\t{len(loader.dst_records):6}{"":4}DSTs')
     print(f'\t{len(loader.plane_records):6}{"":4}Planes')
     print(f'\t{len(loader.plane_type_records):6}{"":4}Plane types')
