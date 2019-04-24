bb731f68e86efd4781b1ad8423dce080acd542f4
BOUILLON Pierre <pierre.bouillon@telecomnancy.eu>
2019-04-24 07:39:37 +0200

    initialize source file

---
+++ DatabaseNormalizer/db_normalizer/data_loader/api_external.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+    db_normalizer.data_loader.api_external
+    --------------------------------------
+
+    TODO doc
+
+    :authors: Bouillon Pierre, Cesari Alexandre.
+    :licence: MIT, see LICENSE for more details.
+"""
+from db_normalizer.data_loader.utils.table_objects import City, Country, Plane
+
+
+def fill_city(city: City) -> None:
+    """TODO doc
+
+    :param city:
+    :return:
+    """
+    # TODO method
+    pass
+
+
+def fill_country(country: Country) -> None:
+    """TODO doc
+
+    :param country:
+    :return:
+    """
+    # TODO method
+    pass
+
+
+def fill_plane(plane: Plane) -> None:
+    """TODO doc
+
+    :param plane:
+    :return:
+    """
+    # TODO: method
+    pass
