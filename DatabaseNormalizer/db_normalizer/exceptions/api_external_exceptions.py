3d15d96effaec8f75080f113e5e0317e1d5f0775
BOUILLON Pierre <pierre.bouillon@telecomnancy.eu>
2019-04-24 13:39:39 +0200

    country loading

---
+++ DatabaseNormalizer/db_normalizer/exceptions/api_external_exceptions.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+    db_normalizer.exception.api_external_exceptions
+    -----------------------------------------------
+
+    Custom exceptions for external loading operations.
+
+    :authors: Bouillon Pierre, Cesari Alexandre.
+    :licence: MIT, see LICENSE for more details.
+"""
+
+
+class UnableToReachCountryApiException(Exception):
+    """TODO
+    """
+    pass
