f0b5a1a3a0b7b484c99ddb3565b7661141a5f039
BOUILLON Pierre <pierre.bouillon@telecomnancy.eu>
2019-04-28 18:59:14 +0200

    Update database_normalizer.py

--- DatabaseNormalizer/db_normalizer/database_normalizer.py
+++ DatabaseNormalizer/db_normalizer/database_normalizer.py
@@ -14,7 +14,7 @@ from db_normalizer.csv_handler.normalizer import Normalizer
 from db_normalizer.csv_handler.utils import Dat
 from db_normalizer.data_loader.loader import Loader
 
-__version__ = '1.8.0'
+__version__ = '1.8.4'
 
 
 def show_header() -> None:
