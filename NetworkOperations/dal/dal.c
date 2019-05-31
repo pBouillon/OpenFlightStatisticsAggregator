c90a3084ade688e27fbc746f2d7fdb2ab1bb2593
pBouillon <pro.pierre.bouillon@protonmail.com>
2019-05-31 08:19:35 +0200

    core structure of the dal

--- NetworkOperations/dal/dal.c
+++ NetworkOperations/dal/dal.c
@@ -1,3 +1,5 @@
+#include <sqlite3.h>
+
 #include "dal.h"
 
 /**
@@ -6,7 +8,51 @@
  *
  * \return 0 on success
  */
-int connect()
+sqlite3 *connect()
 {
-    return 0 ;
+    int rc ;
+    sqlite3 *db ;
+    char *zErrMsg = 0 ;
+
+    rc = sqlite3_open(DB_PATH, &db) ;
+
+    if (rc)
+    {
+        perror("unable to reach the database") ;
+        exit(DB_ERROR_CONENCT) ;
+    }
+
+    return db ;
 } /* connect */
+
+/**
+ * \fn fetch
+ * \brief fetch some rows of a specefic column in a table
+ * 
+ * \param table table to query
+ * \column column column we are looking for
+ * \limit number of records to return
+ * 
+ * \return TODO
+ */
+char **fetch(char *table, char *column, int limit)
+{
+    char results[DB_MAX_ROW][DB_MAX_ROW_LEN] ;
+
+    if (limit > DB_MAX_ROW)
+    {
+        perror("malformatted request") ;
+        exit(DB_ERROR_REQUEST) ;
+    }
+
+    // reach the database
+    sqlite3 *db = connect() ;
+
+    // TODO: perform query
+    // see: http://www.tutorialspoint.com/sqlite/sqlite_c_cpp.htm
+
+    // close the connection
+    sqlite3_close(db) ;
+
+    return results;
+} /* fetch */
--- NetworkOperations/dal/dal.h
+++ NetworkOperations/dal/dal.h
@@ -1,10 +1,21 @@
 #ifndef NETWORKOPERATIONS_DAL_H
 #define NETWORKOPERATIONS_DAL_H
 
+/* --- constants --- */
+// custom errors
+#define     DB_ERROR_CONENCT    1
+#define     DB_ERROR_REQUEST    2
+
 // sqlite default database path
-#define     DB_PATH     "./"
+#define     DB_PATH             "./"
+#define     DB_MAX_ROW          2500
+#define     DB_MAX_ROW_LEN      1024
 
+/* --- methods --- */
+// connect to the database
+sqlite3 *connect();
 
-int connect() ;
+// fetch some rows of a specefic column in a table
+char **fetch(char *table, char *column, int limit) ;
 
 #endif //NETWORKOPERATIONS_DAL_H
